c9c9037abe8723065b4a6843643b006fbb1af5b7
Михаил Никонов <michanik-02@mail.ru>
2022-02-22 10:14:56 +0300

    Добавьте файлы проекта.

---
+++ Lab04-Nikonov/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Lab04_Nikonov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_Element = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_Show = new System.Windows.Forms.Button();
+            this.pb_box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_Element
+            // 
+            this.cb_Element.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_Element.FormattingEnabled = true;
+            this.cb_Element.Location = new System.Drawing.Point(130, 16);
+            this.cb_Element.Name = "cb_Element";
+            this.cb_Element.Size = new System.Drawing.Size(183, 29);
+            this.cb_Element.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Elements";
+            // 
+            // tb_Show
+            // 
+            this.tb_Show.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_Show.Location = new System.Drawing.Point(319, 12);
+            this.tb_Show.Name = "tb_Show";
+            this.tb_Show.Size = new System.Drawing.Size(222, 34);
+            this.tb_Show.TabIndex = 12;
+            this.tb_Show.Text = "Show";
+            this.tb_Show.UseVisualStyleBackColor = true;
+            this.tb_Show.Click += new System.EventHandler(this.tb_Show_Click);
+            // 
+            // pb_box
+            // 
+            this.pb_box.Location = new System.Drawing.Point(21, 59);
+            this.pb_box.Name = "pb_box";
+            this.pb_box.Size = new System.Drawing.Size(520, 379);
+            this.pb_box.TabIndex = 15;
+            this.pb_box.TabStop = false;
+            this.pb_box.Paint += new System.Windows.Forms.PaintEventHandler(this.pb_box_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 450);
+            this.Controls.Add(this.pb_box);
+            this.Controls.Add(this.cb_Element);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Show);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_Element;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button tb_Show;
+        private System.Windows.Forms.PictureBox pb_box;
+    }
+}
+
---
+++ Lab04-Nikonov/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04_Nikonov
+{
+    public partial class Form1 : Form
+    {
+        string connectingstring = @"Data Source=DESKTOP-NOJI3BG;Initial Catalog=DBLab3;Integrated Security=True";
+        SqlCommand cmd = new SqlCommand();
+
+        struct elementinfo
+        {
+            public int atomic;
+            public string name;
+        }
+
+        struct lineinfo
+        {
+            public float wlen;
+            public float intens;
+        }
+
+        List<elementinfo> elements = new List<elementinfo>();
+        List<lineinfo> lines = new List<lineinfo>();
+
+        Pen pen = new Pen(Color.White);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pb_box_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Black);
+
+            int w = pb_box.Width - 1;
+            int h = pb_box.Height - 1;
+
+            float[] c = new float[3];
+            for (int i = 0; i < lines.Count; i++)
+            {
+                float x = Interp.map(lines[i].wlen, 380.0f, 780.0f, 0, w);
+                Wavelength.RGB(lines[i].wlen, c);
+                pen.Color = Color.FromArgb((int)(c[0] * 255.0f), (int)(c[1] * 255.0f), (int)(c[2] * 255.0f));
+                e.Graphics.DrawLine(pen, x, 0, x, h);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            using (SqlConnection connect = new SqlConnection(connectingstring))
+            {
+                connect.Open();
+
+                string sql = "SELECT Atomic_number, Full_name FROM elements;";
+                cmd = new SqlCommand(sql, connect);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                int col_atom = reader.GetOrdinal("atomic_number");
+                int col_name = reader.GetOrdinal("full_name");
+
+                while (reader.Read() == true)
+                {
+                    elementinfo el;
+                    el.atomic = reader.GetInt32(col_atom);
+                    el.name = reader.GetString(col_name);
+
+                    elements.Add(el);
+;                   cb_Element.Items.Add(el.name);
+                }
+                reader.Close();
+
+                connect.Close();
+            }
+
+        }
+
+        private void tb_Show_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connect = new SqlConnection(connectingstring))
+            {
+                int idx = cb_Element.SelectedIndex;
+                if (idx < 0) return;
+
+                int atomic = elements[idx].atomic;
+
+                connect.Open();
+                string sql2 = "SELECT wave_length, rel_intensity FROM spectral_lines ";
+                sql2 += $"WHERE atomic_number = {atomic.ToString()}";
+                                
+                cmd = new SqlCommand(sql2, connect);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                int col_wlen = reader.GetOrdinal("wave_length");
+                int col_reli = reader.GetOrdinal("rel_intensity");
+
+                lines.Clear();
+
+                while (reader.Read() == true)
+                {
+                    lineinfo li;
+
+                    li.wlen = (float)reader.GetDouble(col_wlen);
+                    li.intens = (float)reader.GetDouble(col_reli);
+                    
+                    lines.Add(li);
+                }
+
+                reader.Close();
+                pb_box.Invalidate();
+
+            }
+        }
+    }
+}
